8670eb9de168853789756c5392787c672b820449
schreinereyang <141044326+schreinereyang@users.noreply.github.com>
2025-06-08 19:49:25 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '@/styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />import '@/styles/globals.css'
-
-export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
