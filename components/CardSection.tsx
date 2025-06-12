5aacadaf85d40c9605041a329e4a71439b3666d8
schreinereyang <141044326+schreinereyang@users.noreply.github.com>
2025-06-12 23:41:50 +0200

    Update CardSection.tsx

--- components/CardSection.tsx
+++ components/CardSection.tsx
@@ -3,15 +3,12 @@ import { ReactNode } from "react";
 interface CardSectionProps {
   children: ReactNode;
   className?: string;
-  onClick?: () => void; // 👈 optionnel
+  onClick?: () => void;
 }
 
 export default function CardSection({ children, className = "", onClick }: CardSectionProps) {
   return (
-    <div
-      className={`p-4 bg-[#121827] rounded-2xl shadow-md border border-purple-800 ${className}`}
-      onClick={onClick}
-    >
+    <div className={`p-4 bg-[#121827] rounded-2xl shadow-md border border-purple-800 ${className}`} onClick={onClick}>
       {children}
     </div>
   );
